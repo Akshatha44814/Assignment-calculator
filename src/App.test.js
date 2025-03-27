3bad1722862357f1a2b649d6a9b98de5285f7ec2
Akshatha M <akshatham9@gmail.com>
2025-03-27 14:36:34 +0530

    render Calculate component

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,13 @@
 import { render } from "@testing-library/react";
 import App from "./App";
+import Calculator from "./Calculator/Calculator";
 
 test("renders App Component", () => {
   render(<App />);
+});
+
+describe("renders Calculate Component", () => {
+  it("render Calculate component", () => {
+    render(<Calculator />);
+  });
 });
