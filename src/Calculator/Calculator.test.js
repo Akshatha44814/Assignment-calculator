e58659535253da760f8c425b1005652f77f3b92b
Akshatha M <akshatham9@gmail.com>
2025-03-27 14:53:23 +0530

    onchange input field show input string value

--- src/Calculator/Calculator.test.js
+++ src/Calculator/Calculator.test.js
@@ -1,4 +1,4 @@
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import CalculatorPage from "../CalculatorElement/CalculatorElement";
 
 afterEach(cleanup);
@@ -6,5 +6,19 @@ afterEach(cleanup);
 describe("renders Calculate Component", () => {
   it("Render Calculate Body Component", () => {
     render(<CalculatorPage />);
+  });
+  it("Input string value Onchange event and Onclick calculate button show output in the result output field", () => {
+    render(<CalculatorPage />);
+    const inputStringField = screen.getByPlaceholderText(
+      "E.G.,//;\\n1;2;3 or 1,2,3"
+    );
+    expect(inputStringField).toBeInTheDocument();
+    const value = "1,2,3";
+    fireEvent.change(inputStringField, {
+      target: {
+        value,
+      },
+    });
+    expect(inputStringField).toHaveValue("1,2,3");
   });
 });
